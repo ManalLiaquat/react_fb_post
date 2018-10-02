9d985f92d3e9b9e4a2fec304a3bd2a281ae75f55
Manal Liaquat <m.manalliaquat@gmail.com>
2018-10-03 01:43:29 +0500

    Setting the UI and displaying the info by getting props

--- src/Components/FB_Post/FB_Post.js
+++ src/Components/FB_Post/FB_Post.js
@@ -1,86 +1,96 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import classnames from 'classnames';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import red from '@material-ui/core/colors/red';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+// other libs
+import FacebookEmoji from 'react-facebook-emoji';
+import FacebookImage from "react-fb-image-grid";
 
 const styles = theme => ({
   card: {
-    maxWidth: 400,
-  },
-  media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
+    maxWidth: 600,
+    margin: '5px',
+    padding: '0px'
   },
   actions: {
     display: 'flex',
-  },
-  avatar: {
-    // backgroundColor: red[500],
-
-  },
+  }
 });
 
 class FBPost extends React.Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      showEmoji: false
+    }
   }
 
   render() {
-    const { classes } = this.props;
+    const { classes, post } = this.props;
+    const { showEmoji } = this.state
+    console.log("post****", post);
 
     return (
-      <Card className={classes.card}>
-        <CardHeader
-          avatar={
-            <Avatar aria-label="Recipe" className={classes.avatar}>
-              <img src="https://pbs.twimg.com/profile_images/711491232980148224/Sqh_Tbh7_400x400.jpg" alt="" width="40px" height="40px" />
-            </Avatar>
+      <div>
+
+        <Card className={classes.card}>
+
+          <CardHeader
+            avatar={
+              <Avatar aria-label="Profile Picture">
+                <img src={post.avatar} alt="" width="40px" height="40px" />
+              </Avatar>
+            }
+            action={
+              <IconButton>
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title={post.createdBy}
+            subheader={post.createdAt}
+          />
+          <CardContent>
+            <Typography component="p">
+              {post.description}
+            </Typography>
+          </CardContent>
+          <div>
+            <FacebookImage width={100} countFrom={5} images={post.images} />
+          </div>
+          <hr />
+          <p style={{ fontSize: "10px", marginLeft: '2px' }}>{post.likes[0]}, {post.likes[1]} and {post.likes.length - 2} others</p>
+          {
+            showEmoji ? <div>
+              <FacebookEmoji size="sm" type="like" />
+              <FacebookEmoji size="sm" type="love" />
+              <FacebookEmoji size="sm" type="wow" />
+              <FacebookEmoji size="sm" type="yay" />
+              <FacebookEmoji size="sm" type="angry" />
+              <FacebookEmoji size="sm" type="haha" />
+              <FacebookEmoji size="sm" type="sad" />
+            </div> : null
           }
-          action={
-            <IconButton>
-              <MoreVertIcon />
+          <CardActions className={classes.actions} disableActionSpacing>
+            <IconButton aria-label="Add to favorites">
+              <FavoriteIcon onClick={() => { this.setState({ showEmoji: !showEmoji }) }} />
             </IconButton>
-          }
-          title="Shrimp and Chorizo Paella"
-          subheader="September 14, 2016"
-        />
-        <CardMedia
-          className={classes.media}
-          image="https://cdn.lifehack.org/wp-content/uploads/2013/04/33.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography component="p">
-            This impressive paella is a perfect party dish and a fun meal to cook together with your
-            guests. Add 1 cup of frozen peas along with the mussels, if you like.
-          </Typography>
-        </CardContent>
-        <hr />
-        <p style={{ fontSize: "10px", marginLeft: '2px' }}>google</p>
-        <CardActions className={classes.actions} disableActionSpacing>
-          <IconButton aria-label="Add to favorites">
-            <FavoriteIcon />
-          </IconButton>
-          <IconButton aria-label="Share">
-            <ShareIcon />
-          </IconButton>
-        </CardActions>
-      </Card>
+            <IconButton aria-label="Share">
+              <ShareIcon />
+            </IconButton>
+          </CardActions>
+        </Card>
+
+      </div>
     );
   }
 }
