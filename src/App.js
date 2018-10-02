162fda830a9d9d4af64a81899db0b7815faf42b4
Manal Liaquat <m.manalliaquat@gmail.com>
2018-10-02 21:58:41 +0500

    Setting UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import SimpleAppBar from "./Components/Navbar/Navbar";
+import FBPost from "./Components/FB_Post/FB_Post";
 
 // material ui
-// import { Button, Typography } from "@material-ui/core";
-// import NavigateNext from "@material-ui/icons/NavigateNext";
+import { Button, Typography, Grid, Paper } from "@material-ui/core";
+import NavigateNext from "@material-ui/icons/NavigateNext";
 // material ui
 
 class App extends Component {
@@ -15,9 +17,21 @@ class App extends Component {
   }
 
   render() {
+    const paperStyle = { padding: "20px", margin: "20px" };
     return (
-      <div className="App">
-        kuch  to  hoga  hai  yaha
+      <div>
+        <SimpleAppBar />
+        <Grid container spacing={24} style={paperStyle} direction="row" justify="space-evenly" alignItems="center">
+          {/* <Grid item xs={6}>
+            <Paper style={paperStyle}>Inputs here</Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper style={paperStyle}>
+            </Paper>
+          </Grid> */}
+          <FBPost></FBPost>
+        </Grid>
+        {/* <Button variant="contained" color="primary" >Click</Button> */}
       </div>
     );
   }
---
+++ src/Components/FB_Post/FB_Post.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import classnames from 'classnames';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import red from '@material-ui/core/colors/red';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+const styles = theme => ({
+  card: {
+    maxWidth: 400,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  actions: {
+    display: 'flex',
+  },
+  avatar: {
+    // backgroundColor: red[500],
+
+  },
+});
+
+class FBPost extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Card className={classes.card}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="Recipe" className={classes.avatar}>
+              <img src="https://pbs.twimg.com/profile_images/711491232980148224/Sqh_Tbh7_400x400.jpg" alt="" width="40px" height="40px" />
+            </Avatar>
+          }
+          action={
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title="Shrimp and Chorizo Paella"
+          subheader="September 14, 2016"
+        />
+        <CardMedia
+          className={classes.media}
+          image="https://cdn.lifehack.org/wp-content/uploads/2013/04/33.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography component="p">
+            This impressive paella is a perfect party dish and a fun meal to cook together with your
+            guests. Add 1 cup of frozen peas along with the mussels, if you like.
+          </Typography>
+        </CardContent>
+        <hr />
+        <p style={{ fontSize: "10px", marginLeft: '2px' }}>google</p>
+        <CardActions className={classes.actions} disableActionSpacing>
+          <IconButton aria-label="Add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="Share">
+            <ShareIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
+    );
+  }
+}
+
+FBPost.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(FBPost);
