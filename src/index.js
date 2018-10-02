a158efa109fe8c2aba17e1f42ae946ceca2f4796
Manal Liaquat <m.manalliaquat@gmail.com>
2018-10-03 01:44:02 +0500

    using module.hot

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+if (module.hot) {
+    module.hot.accept()
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
