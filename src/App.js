923d3d1edc6d7d7c3bbf453ee9bfe735957039be
Manal Liaquat <m.manalliaquat@gmail.com>
2018-10-03 22:37:07 +0500

    checking and testing app

--- src/App.js
+++ src/App.js
@@ -30,7 +30,8 @@ class App extends Component {
     return (
       <div>
         <SimpleAppBar />
-        <FacebookImage images={post_obj.images} width={30} />
+        {/* <FacebookImage images={post_obj.images} width={30} /> */}
+        {/* <hr /> */}
         <Grid container style={paperStyle} direction="row" justify="space-evenly" alignItems="center">
           <div style={paperStyle}>
             <FBPost post={post_obj} />
